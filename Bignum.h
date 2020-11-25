3bbd14cb318a281fc32488356ad4ede7fce23ecd
xsw0 <xsw.mail.0@gmail.com>
2020-11-26 00:21:11 +0800

    opposite, abs

--- Bignum.h
+++ Bignum.h
@@ -129,6 +129,37 @@ public:
         result >>= n;
         return result;
     }
+
+private:
+    void opposite()
+    {
+        switch (sign)
+        {
+        case Sign::negative:
+            sign = Sign::positive;
+            return;
+        case Sign::positive:
+            sign = Sign::negative;
+            return;
+        default:
+            return;
+        }
+    }
+
+public:
+    Bignum operator-() const
+    {
+        auto result = *this;
+        result.opposite();
+        return result;
+    }
+
+    Bignum abs() const
+    {
+        auto result = *this;
+        result.sign = Sign::positive;
+        return result;
+    }
 };
 
 #endif
